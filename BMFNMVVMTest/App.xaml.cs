77d37249c0ce7a8bf02f1ec8770b25e096a85dcc
sergiytereshchenko <sergiyter@gmail.com>
2014-05-21 18:20:07 +0400

    InitialCommit

---
+++ BMFNMVVMTest/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight.Threading;
+
+namespace BMFNMVVMTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            DispatcherHelper.Initialize();
+        }
+    }
+}
