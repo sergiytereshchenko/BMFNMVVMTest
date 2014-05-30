5aed5e25101862eb8bf99dc89b5d71c3c328f9d8
sergiytereshchenko <sergiyter@gmail.com>
2014-05-30 18:18:43 +0400

    20140530DataInitialization changed

--- BMFNMVVMTest/Model/ReportsContext.cs
+++ BMFNMVVMTest/Model/ReportsContext.cs
@@ -33,6 +33,9 @@ namespace BMFNMVVMTest.Model
             get { return reportsParser; }
         }
 
+        /// <summary>
+        /// All reports types must be listed here
+        /// </summary>
         private ReportsContext()
         {
             listTypes = new List<Type>() {typeof(Report1), typeof(Report2)};
@@ -60,8 +63,6 @@ namespace BMFNMVVMTest.Model
             }
         }
 
-
-
         private void dataInitialization()
         {
             testData = new List<Object>();
@@ -71,71 +72,43 @@ namespace BMFNMVVMTest.Model
             List<int> curList;
             ArrayList curArrayList;
 
+            for (int i = 1; i < 11; i++)
+            {
+                curReport = new Report1();
+                ((Report1)curReport).Field1 = i*22.33;
+                curSubReport = new SubReport();
+                curSubReport.Field7 = i * 12.0001;
+                curSubReport.Field8 = i * 1200;
+                curSubReport.Field9 = i * 121;
+                curSubReport.Field10 = String.Format("Subrep{0}", i);
+                curArrayList = new ArrayList();
+                curArrayList.Add(i * 10);
+                curArrayList.Add(i * 20.20);
+                curArrayList.Add("str30");
+                curArrayList.Add(i * 40);
+                curSubReport.Field11 = curArrayList;
+                ((Report1)curReport).Field2 = curSubReport;
+                ((Report1)curReport).Field3 = String.Format("Rep1-obj{0}", i);
+                ((Report1)curReport).Field4 = i * 8;
+                ((Report1)curReport).Field5 = i * 99999.001;
+                curList = new List<int>();
+                curList.Add(11);
+                curList.Add(22);
+                curList.Add(33);
+                ((Report1)curReport).Field6 = curList;
 
-            curReport = new Report1();
-            ((Report1)curReport).Field1 = 22.33;
-            curSubReport = new SubReport();
-            curSubReport.Field7 = 12.0001;
-            curSubReport.Field8 = 1200;
-            curSubReport.Field9 = 121;
-            curSubReport.Field10 = "Subrep1";
-            curArrayList = new ArrayList();
-            curArrayList.Add(10);
-            curArrayList.Add(20.20);
-            curArrayList.Add("str30");
-            curArrayList.Add(40);
-            curSubReport.Field11 = curArrayList;
-            ((Report1)curReport).Field2 = curSubReport;
-            ((Report1)curReport).Field3 = "Rep1-obj1";
-            ((Report1)curReport).Field4 = 8;
-            ((Report1)curReport).Field5 = 99999.001;
-            curList = new List<int>();
-            curList.Add(11);
-            curList.Add(22);
-            curList.Add(33);
-            ((Report1)curReport).Field6 = curList;
+                testData.Add(curReport);
 
-            testData.Add(curReport);
+                curReport = new Report2();
+                ((Report2)curReport).Field1 = i * 42.663;
+                ((Report2)curReport).Field2 = new Point(i * 3, i * 5);
+                ((Report2)curReport).Field3 = String.Format("Rep2-obj{0}", i);
+                ((Report2)curReport).Field4 = i * 7;
+                ((Report2)curReport).Field5 = i * 3423.423;
+                ((Report2)curReport).Field6 = new string[] { String.Format("str1{0}", i), String.Format("ste2{0}", i), String.Format("str3{0}", i) };
 
-            curReport = new Report1();
-            ((Report1)curReport).Field1 = 42.663;
-            curSubReport = new SubReport();
-            curSubReport.Field7 = 24.0001;
-            curSubReport.Field8 = 2400;
-            curSubReport.Field9 = 241;
-            curSubReport.Field10 = "Subrep2";
-            curArrayList = new ArrayList();
-            curArrayList.Add(1010);
-            curArrayList.Add(2020.2020);
-            curArrayList.Add("str3030");
-            curArrayList.Add(4040);
-            curSubReport.Field11 = curArrayList;
-            ((Report1)curReport).Field2 = curSubReport;
-            ((Report1)curReport).Field3 = "Rep1-obj2";
-            ((Report1)curReport).Field4 = 7;
-            ((Report1)curReport).Field5 = 3423423;
-            curList = new List<int>();
-            curList.Add(111);
-            curList.Add(222);
-            curList.Add(333);
-            ((Report1)curReport).Field6 = curList;
-
-
-            testData.Add(curReport);
-
-
-            curReport = new Report2();
-            ((Report2)curReport).Field1 = 42.663;
-            ((Report2)curReport).Field2 = new Point(33, 55);
-            ((Report2)curReport).Field3 = "Rep2-obj1";
-            ((Report2)curReport).Field4 = 7;
-            ((Report2)curReport).Field5 = 3423.423;
-            ((Report2)curReport).Field6 = new string[] { "str1", "str2", "str3" };
-
-            testData.Add(curReport);
-
-
-
+                testData.Add(curReport);
+            }
         }
     }
 }
